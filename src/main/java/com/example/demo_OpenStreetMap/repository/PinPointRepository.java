e8e8fc550f58327ad8416e367249cdf4c2b3a586
RobinHagolani <robin.hagolani@dci-student.org>
2024-11-26 14:24:01 +0100

    modified with bulma and added new functions

--- src/main/java/com/example/demo_OpenStreetMap/repository/PinPointRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo_OpenStreetMap.repository;
-
-import com.example.demo_OpenStreetMap.model.PinPoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PinPointRepository extends JpaRepository<PinPoint, Long> {
-
-}
